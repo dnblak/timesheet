45a25d39bd06de438d2a8824abfaf63c79093630
Daniel Blake <dblake@zai-inc.com>
2016-07-07 18:32:08 -0400

    updated dbsetup.py

--- dbsetup.py
+++ dbsetup.py
@@ -2,24 +2,26 @@ import sqlite3
 
 sqlite_file = '/Volumes/ZAI-Enrypted/notes/timesheet/timesheet.sqlite'
 
-table_name1 = 'my_table_1'  # name of the table to be created
-table_name2 = 'my_table_2'  # name of the table to be created
+table_name1 = 'timelog'  # name of the table to be created
 
-new_field = 'my_1st_column' # name of the column
+new_field = 'ID' # name of the column
 field_type = 'INTEGER'  # column data type
+nf01 = 'RFID'
+ft01 = 'TEXT'
+nf02 = 'tstamp'
+ft02 = 'INTEGER'
+nf03 = 'inout'
+ft03 = 'INTEGER'
+
 
 # Connecting to the database file
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
 # Creating a new SQLite table with 1 column
-c.execute('CREATE TABLE {tn} ({nf} {ft})'\
-        .format(tn=table_name1, nf=new_field, ft=field_type))
+c.execute('''CREATE TABLE timelog 
+	(ID INTEGER PRIMARY KEY, RFID TEXT, tstamp INTEGER, inout INTEGER)''')
 
-# Creating a second table with 1 column and set it as PRIMARY KEY
-# note that PRIMARY KEY column must consist of unique values!
-c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
-        .format(tn=table_name2, nf=new_field, ft=field_type))
 
 # Committing changes and closing the connection to the database file
 conn.commit()
